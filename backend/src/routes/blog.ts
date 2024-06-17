f39de19aadab37b2a597d981abc2209134687006
Sunny <swarnendughosh05@gmail.com>
2024-06-17 18:25:25 +0530

    author name fixed

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -128,6 +128,16 @@ blogRouter.get("/:id", async (c)=> {
         where: {
             id: Number(id)
         }, 
+        select: {
+            id:true,
+            title: true,
+            content: true,
+            author: {
+                select: {
+                    name: true,
+                }
+            }
+        }
     })
     return c.json({
         blog
